48daecdff482c5d21719b4e4f15983fc8269674f
Juhyeong Park <majecty@gmail.com>
2019-06-10 20:58:41 +0900

    MOve spoon and fork

--- Assets/Scripts/Table/Table.cs
+++ Assets/Scripts/Table/Table.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Table : MonoBehaviour
 {
     [SerializeField] private Plate plate;
+    [SerializeField] private ForkAndSpoon forkAndSpoon;
 
     public void Show()
     {
@@ -25,5 +26,11 @@ public class Table : MonoBehaviour
             to: new Vector3(0, 0, plate.transform.position.z),
             time: 1f,
             transform: plate.transform);
+
+        yield return TweenUtil.Move(
+            from: forkAndSpoon.transform.position,
+            to: new Vector3(0, 0, forkAndSpoon.transform.position.z),
+            time: 0.3f,
+            transform: forkAndSpoon.transform);
     }
 }
